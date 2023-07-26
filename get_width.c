bafc16355dc7ef16f5b6b2668c6c0ef97534f23b
FRESHMEMBA <mazibukosibonelo@gmail.com>
2023-07-25 20:00:51 -0700

    Changes made: get_width.c:36:1 Inserted a closing curly brace | utils.c:23 Iserted a comma between the variable 'ascii_code' and the keyword 'char'| utils.c:58 Changed SIZE to size | Changed moved the comma placed after the variable 'num' to be before 'num'

--- get_width.c
+++ get_width.c
@@ -33,3 +33,4 @@ int get_width(const char *format, int *i, va_list list)
 	*i = current - 1;
 
 	return (width);
+}
--- utils.c
+++ utils.c
@@ -20,7 +20,7 @@ int is_printable(char c)
  * @ascii_code: ASCII code.
  * Return: Always 3
  */
-int append_hexa_code(char ascii_code char buffer[], int i)
+int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
 	/*The hexa format code is always 2 digits long*/
@@ -55,7 +55,7 @@ int is_digit(char c)
  */
 long int convert_size_number(long int num, int size)
 {
-	if (SIZE == S_LONG)
+	if (size == S_LONG)
 		return (num);
 	else if (size == S_SHORT)
 		return ((short)num);
@@ -68,7 +68,7 @@ long int convert_size_number(long int num, int size)
  * @size: Number indicating the type to be casted.
  * Return: casted value of num.
  */
-long int convert_size_unsgnd(unsigned long, int num, int size)
+long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
